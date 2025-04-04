b99589eb172f6e71472896b3a2b0eaad6a07452b
GoodGuyBabbl <leon.bablick@gmx.de>
2025-04-04 13:29:20 +0200

    v0.2

--- GhostMansion/Assets/Scripts/MainCharacter/Movement.cs
+++ GhostMansion/Assets/Scripts/MainCharacter/Movement.cs
@@ -4,6 +4,8 @@ public class Movement : MonoBehaviour
 {
     public Animator animator;
 
+    private Vector3 LastMoveDirection = Vector3.down;
+
     
     void Start()
     {
@@ -15,8 +17,30 @@ public class Movement : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        if(movement.magnitude > 0.0f)
+        {
+            LastMoveDirection = movement;
+        }
+
+
+        Vector3 IdleDirection;
+
+        if (movement.magnitude > 0.0f)
+        {
+            IdleDirection = movement;
+        }
+        else
+        {
+            IdleDirection = LastMoveDirection;
+        }
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("XIdleDirection", IdleDirection.x);
+        animator.SetFloat("YIdleDirection", IdleDirection.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
 
         transform.position +=  movement * Time.deltaTime;
     }
